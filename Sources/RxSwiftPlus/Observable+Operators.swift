4408640a80fdc2ca4330bb373ee2fbf712df26d7
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-06-12 23:10:33 +0800

    Updates

--- Sources/RxSwiftPlus/Observable+Operators.swift
+++ Sources/RxSwiftPlus/Observable+Operators.swift
@@ -42,6 +42,34 @@ extension Observable where Element: OptionalType {
     }
 }
 
+extension SharedSequenceConvertibleType where Element: OptionalType {
+    public func filterNil() -> SharedSequence<SharingStrategy, Element.Wrapped> {
+        return flatMap { element -> SharedSequence<SharingStrategy, Element.Wrapped> in
+            if let value = element.value {
+                return .just(value)
+            } else {
+                return .empty()
+            }
+        }
+    }
+
+    public func filterNilKeepOptional() -> SharedSequence<SharingStrategy, Element> {
+        return filter { element -> Bool in
+            return element.value != nil
+        }
+    }
+
+    public func replaceNil(with nilValue: Element.Wrapped) -> SharedSequence<SharingStrategy, Element.Wrapped> {
+        return flatMap { element -> SharedSequence<SharingStrategy, Element.Wrapped> in
+            if let value = element.value {
+                return .just(value)
+            } else {
+                return .just(nilValue)
+            }
+        }
+    }
+}
+
 public protocol BooleanType {
     var boolValue: Bool { get }
 }
@@ -71,6 +99,36 @@ extension ObservableType where Element == Bool {
     /// Boolean not operator
     public func not() -> Observable<Bool> {
         return map(!)
+    }
+}
+
+/// Maps true to false and vice versa
+extension SharedSequenceConvertibleType where Element: BooleanType {
+    public func not() -> SharedSequence<SharingStrategy, Bool> {
+        return map { input in
+            return !input.boolValue
+        }
+    }
+}
+
+extension SharedSequenceConvertibleType where Element: Equatable {
+    public func ignore(value: Element) -> SharedSequence<SharingStrategy, Element> {
+        return filter { selfE -> Bool in
+            return value != selfE
+        }
+    }
+}
+
+extension SharedSequenceConvertibleType where Element == Bool {
+    /// Boolean not operator
+    public func not() -> SharedSequence<SharingStrategy, Bool> {
+        return map(!)
+    }
+}
+
+extension ObservableType where Element == Bool {
+    public func mapToVoid() -> Observable<Void> {
+        return map { _ in }
     }
 }
 

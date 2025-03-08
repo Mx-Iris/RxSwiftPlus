f8aa945524d1d74acec5edd0bacbeed0c099edf5
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2025-03-09 02:11:19 +0800

    Update swift-syntax version

--- Package.swift
+++ Package.swift
@@ -86,7 +86,7 @@ let package = Package(
 extension Package.Dependency {
     static let SwiftSyntax = Package.Dependency.package(
         url: "https://github.com/swiftlang/swift-syntax.git",
-        from: "509.0.0"
+        from: "510.0.0"
     )
     static let Kingfisher = Package.Dependency.package(
         url: "https://github.com/onevcat/Kingfisher",
